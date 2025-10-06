c1f3773aa1b6f8ed9d7d835a21446d4ceb27801f
amanmishra2424 <amanmishraaa767@gmail.com>
2025-10-06 12:44:38 +0530

    for cronjob i added ping controller

---
+++ src/main/java/com/pdfprinting/controller/PingController.java
@@ -0,0 +1,11 @@
+package com.pdfprinting.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "HARI OM";
+    }
+}
